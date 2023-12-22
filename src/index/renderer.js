d1e8a65c12a5693b873d1a95703168c0f7fe0e2b
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2023-12-22 12:45:19 +0530

    Styling the menu page, and adding hardware lock boilerplate

---
+++ src/index/renderer.js
@@ -0,0 +1,9 @@
+function showSubItems(subMenuId, menuId) {
+    // Hide all sub-menus
+    document.querySelectorAll('.sub-menu').forEach(menu => menu.style.display = 'none');
+    document.querySelectorAll('.menuItem').forEach(item => item.classList.remove('active'));
+    // Show the selected sub-menu
+    document.getElementById(subMenuId).style.display = 'block';
+    document.getElementById(menuId).classList.add('active');
+  }
+  
--- src/main.js
+++ src/main.js
@@ -1,7 +1,35 @@
 const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
+const usb = require('usb');
+const usbDetect = require('usb-detection');
+usbDetect.startMonitoring();
 
 let mainWindow;
+
+const USB_SERIAL_NUMBER = '007604D63040';
+
+usbDetect.on('add', () => {
+  const devices = usb.getDeviceList();
+  let found = devices.some(device => {
+    try {
+      device.open();
+      const serialNumber = device.deviceDescriptor.iSerialNumber;
+      device.close();
+      return serialNumber === USB_SERIAL_NUMBER;
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  });
+
+  if (found) {
+    console.log("USB Key Found. App starting...");
+    // Load your app here
+  } else {
+    console.log("USB Key not found. Exiting...");
+    app.quit();
+  }
+});
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -10,12 +38,12 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true
     },
-    icon: path.join(__dirname, 'shared/img/nvisLogo.ico')
+    icon: path.join(__dirname, '../icon.ico')
   });
 
   // mainWindow.setMenu(null);
 
-  mainWindow.loadFile(path.join(__dirname, 'index.html')).catch(err => {
+  mainWindow.loadFile(path.join(__dirname, 'home.html')).catch(err => {
     console.error('Failed to load index.html:', err);
   });
   
