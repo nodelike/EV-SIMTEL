66d64b88d189aa40641cf853a3e72de0a44173b7
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2023-12-23 11:03:36 +0530

    Changed the file structure and styled the inital menus

--- src/index/renderer.js
+++ src/index/renderer.js
@@ -1,9 +1,8 @@
 function showSubItems(subMenuId, menuId) {
-    // Hide all sub-menus
-    document.querySelectorAll('.sub-menu').forEach(menu => menu.style.display = 'none');
-    document.querySelectorAll('.menuItem').forEach(item => item.classList.remove('active'));
-    // Show the selected sub-menu
-    document.getElementById(subMenuId).style.display = 'block';
-    document.getElementById(menuId).classList.add('active');
-  }
-  
+  // Hide all sub-menus
+  document.querySelectorAll('.sub-menu').forEach(menu => menu.style.display = 'none');
+  document.querySelectorAll('.menuItem').forEach(item => item.classList.remove('active'));
+  // Show the selected sub-menu
+  document.getElementById(subMenuId).style.display = 'block';
+  document.getElementById(menuId).classList.add('active');
+}
